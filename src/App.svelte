83351cb774b5902281a56faed2ad070be7b8bc0a
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2023-09-23 11:05:26 +0300

    create todo app

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import Todo from './lib/Todo.svelte'
+</script>
+
+<main>
+  <h1>Todo List</h1>
+
+  <div class="card">
+    <Todo />
+  </div>
+</main>
